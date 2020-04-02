ea9fd8e6f562e99b211d1ed82ccbecf75f8fb694
jguhlin <joseph.guhlin@gmail.com>
2020-04-02 20:27:55 +1300

    changes

--- liboracular/src/kmers.rs
+++ liboracular/src/kmers.rs
@@ -105,6 +105,8 @@ impl Iterator for DiscriminatorMaskedGenerator {
     }
 }
 
+// TODO: Split at 3 N's or more...
+// Have fn, just need it to output Sequences...
 impl KmerWindowGenerator {
 
     pub fn new(filename: String,
@@ -162,10 +164,10 @@ impl Iterator for KmerWindowGenerator {
 //            println!("Getting new sequence...");
             let mut curseq: io::Sequence = match self.sequences.next() {
                 Some(x) => x,
-                None    => { println!("Reporting we are out of sequence!"); return None }  // That's it... no more!
+                None    => return None  // That's it... no more!
             };
             
-            println!("Getting new sequence... {}", &curseq.id);
+            // println!("Getting new sequence... {}", &curseq.id);
 
             if self.rc {
                 let io::Sequence { id, mut seq } = curseq;
@@ -181,11 +183,7 @@ impl Iterator for KmerWindowGenerator {
 
             self.curseq = curseq.clone();
             let kmer_generator = Kmers::new(curseq.seq.clone(), self.k, self.offset);
-	    self.kmer_generator = kmer_generator;
-
-//            self.kmer_generator.seq = self.curseq.seq.clone();
-//            self.kmer_generator.curpos = 0;
-//            self.kmer_generator.len = self.kmer_generator.seq.len();
+	        self.kmer_generator = kmer_generator;
         }
 
         let mut kmers: Vec<Vec<u8>> = Vec::with_capacity(self.window_size);
