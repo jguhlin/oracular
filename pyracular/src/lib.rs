4db017a1f90743709a8658531e6f3a0df13089a6
Joseph Guhlin <joseph.guhlin@gmail.com>
2022-09-14 15:52:47 +1200

    .

--- pyracular/src/lib.rs
+++ pyracular/src/lib.rs
@@ -891,8 +891,10 @@ fn get_random_sequence_from_seqloc<R: Rng + ?Sized>(
 
     let mut start = rng.gen_range(0..seqlen);
     let mut end = start + needed_length;
-
+    println!("Seq Length: {}", seqlen);
+    println!("{:#?}", seqloc.sequence.as_ref().unwrap().iter().map(|l| l.original_format(sfasta.parameters.block_size)));
     println!("{:#?}", seqloc);
+    println!("range: {:#?}", start..end);
 
     seq = sfasta.get_sequence_only_by_seqloc(&seqloc.slice(sfasta.parameters.block_size, start..end)).unwrap().unwrap();
 
